0395136509205e95457ee01b48e2ef6a876e8091
royFloresNyc <royfloresnyc@gmail.com>
2020-08-31 19:40:36 -0400

    Complete

--- app/controllers/artist_instruments_controller.rb
+++ app/controllers/artist_instruments_controller.rb
@@ -4,7 +4,8 @@ class ArtistInstrumentsController < ApplicationController
     end 
 
     def create
-
+        @artist_instrument = ArtistInstrument.find_or_create_by(params.require(:artist_instrument).permit(:artist_id, :instrument_id))
+        redirect_to artist_path(@artist_instrument.artist)
     end
 
 end 
--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -12,6 +12,13 @@ class ArtistsController < ApplicationController
     end 
 
     def create
+        @artist = Artist.create(params.require(:artist).permit(:name, :age, :title))
+        if @artist.valid?
+            redirect_to artist_path(@artist)
+        else 
+            flash[:errors] = @artist.errors.full_messages
+            redirect_to new_artist_path
+        end 
     end 
 
 end
